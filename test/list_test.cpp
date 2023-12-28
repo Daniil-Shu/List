d0a8d9ea6910c6ca85f9d4aa80a4ac8859463f7f
Даниил <daniil@MacBook-Air-Daniil.local>
2023-12-28 10:40:31 +0300

    Изменения согласно комментариям

--- test/list_test.cpp
+++ test/list_test.cpp
@@ -17,12 +17,12 @@ TEST(ListTest, ListGrowsAndShrinks) {
     ll1.push_front(8);
     ll1.push(9,2);
 
-    ASSERT_EQ(ll1.getSize(), 4);
+    ASSERT_EQ(ll1.getSize(), 5);
 
     ll1.pop_back();
     ll1.pop_front();
 
-    ASSERT_EQ(ll1.getSize(), 2);
+    ASSERT_EQ(ll1.getSize(), 3);
 }
 
 TEST(ListTest, CopyConstructorTest) {
@@ -33,7 +33,7 @@ TEST(ListTest, CopyConstructorTest) {
     ll1.push_front(8);
     ll1.push(9,2);
 
-    ASSERT_EQ(ll1.getSize(), 4);
+    ASSERT_EQ(ll1.getSize(), 5);
 
     List<int> ll2 = List<int>(ll1);
 
@@ -49,7 +49,7 @@ TEST(ListTest, CopyAssignTest) {
     ll1.push_front(8);
     ll1.push(9,2);
 
-    ASSERT_EQ(ll1.getSize(), 4);
+    ASSERT_EQ(ll1.getSize(), 5);
 
     List<int> ll2 = ll1;
 
@@ -66,7 +66,7 @@ TEST(ListTest, MoveConstructorTest) {
     ll1.push(9,2);
 
     const auto ll1Size = ll1.getSize();
-    ASSERT_EQ(ll1Size, 4);
+    ASSERT_EQ(ll1Size, 5);
 
     List<int> ll2 = List<int>(std::move(ll1));
 
@@ -83,12 +83,38 @@ TEST(ListTest, MoveAssignmentTest) {
     ll1.push(9,2);
 
     const auto ll1Size = ll1.getSize();
-    ASSERT_EQ(ll1Size, 4);
+    ASSERT_EQ(ll1Size, 5);
 
     List<int> ll2 = std::move(ll1);
 
     ASSERT_EQ(ll2.getSize(), ll1Size);
     ASSERT_NE(ll2.getHead(), nullptr);
+}
+
+TEST(ListTest, OverloadOutOperator) {
+    List<int> ll1;
+    ll1.push_back(5);
+    ASSERT_EQ(ll1.getSize(), 1);
+
+    std::stringstream stream;
+    stream << ll1;
+    ASSERT_EQ(stream.str().compare("5"), 1);
+}
+
+TEST(ListTest, TestGetByIndex) {
+    List<int> ll1;
+    int found1 = 5;
+    int found2 = 5;
+
+    ll1.push_back(found1);
+    ll1.push_back(1);
+    ll1.push_back(found2);
+    ll1.push_back(3);
+    ASSERT_EQ(ll1.getSize(), 4);
+
+
+    ASSERT_EQ(ll1.get_by_index(0)->data, found1);
+    ASSERT_EQ(ll1.get_by_index(INT32_MAX), nullptr);
 }
 
 int main(int argc, char **argv) {
